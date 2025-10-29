9dd4c6d4a0f0c06eb0416e356c5c8b80285d9ce6
azharkhairaa <azhar.khairaa@gmail.com>
2025-10-29 10:48:01 +0700

    solve chlg 1 python

--- python/challenges/1_list_five_number.py
+++ python/challenges/1_list_five_number.py
@@ -4,12 +4,16 @@ print("List awal:", numbers)
 
 # 2. TODO: Tampilkan angka pertama dan terakhir
 print("Angka pertama:")
+print(numbers[0])
 print("Angka terakhir:")
+print(numbers[-1])
 
 # 3. TODO: Tambahkan angka baru (apapun) ke akhir list
+numbers.append(55)
 print("List setelah ditambah:", numbers)
 
 # 4. TODO: Urutkan list secara descending
+numbers.sort(reverse=True)
 
 # 5. Tampilkan hasil akhir
 print("List urut descending:", numbers)
